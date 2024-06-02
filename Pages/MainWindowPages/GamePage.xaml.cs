30a21a38b49be5cdf9bd78a2850fa1fee3b1bb2c
zxc <zxc@DESKTOP-3DQKFI9>
2024-06-02 21:58:47 +0500

    finish buy button and eby4iy output document

--- Pages/MainWindowPages/GamePage.xaml.cs
+++ Pages/MainWindowPages/GamePage.xaml.cs
@@ -1,49 +1,279 @@
-﻿using Mist.Helper;
-using Mist.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
+using System.Windows.Threading;
+using Mist.Helper;
+using Mist.Model;
 
 namespace Mist.Pages.MainWindowPages
 {
-    /// <summary>
-    /// Interaction logic for GamePage.xaml
-    /// </summary>
     public partial class GamePage : Page
     {
         public Game Game;
+
         public List<byte[]> GameImages;
         public List<byte[]> GameVideos;
+
+        public GameSysReq GameMinSysReqs;
+        public GameSysReq GameMaxSysReqs;
+
+        private System.Timers.Timer timer;
+        private bool isDragging = false;
+
+        public List<string> videosPath = new List<string>();
+        public List<string> videosPathMini = new List<string>();
         public GamePage(Game game)
         {
             InitializeComponent();
             Game = game;
             GameImages = App.Context.GameImages.Where(x => x.GameId == Game.Id).Select(x => x.Image).ToList();
             GameVideos = App.Context.GameVideos.Where(x => x.GameId == Game.Id).Select(x => x.Video).ToList();
+            GameMinSysReqs = App.Context.GameSysReqs.Where(x => x.GameId == Game.Id && x.Type == "Минимальные").First();
+            GameMaxSysReqs = App.Context.GameSysReqs.Where(x => x.GameId == Game.Id && x.Type == "Рекомендованные").First();
+
             RefreshGame();
+
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+
+            volume_Slider.Value = video_MediaElement.Volume * 100;
+
+            RefreshMedia();
+        }
+
+        private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            if (!isDragging)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    video_Slider.Value = video_MediaElement.Position.TotalSeconds;
+                });
+            }
         }
 
         public void RefreshGame()
         {
-            gameName_Label.Content = Game.Name;
+            gameName_Label.Text = Game.Name;
             front_Image.Source = ImageHelper.GetImage(Game.FrontImage);
             bio_TextBlock.Text = Game.Bio;
+            description_TextBlock.Text = Game.Description;
             releaseDate_Label.Content = Game.ReleaseDate;
             developer_Label.Content = App.Context.Developers.Where(x => x.Id == Game.DeveloperId).First().Name;
 
-            string videoFilePath = SaveVideoToFile(GameVideos.Last(), "video.mp4");
+            osMin_TextBlock.Text = GameMinSysReqs.Os;
+            cpuMin_TextBlock.Text = GameMinSysReqs.Cpu;
+            memMin_TextBlock.Text = GameMinSysReqs.Memory;
+            gpuMin_TextBlock.Text = GameMinSysReqs.Gpu;
+            directxMin_TextBlock.Text = GameMinSysReqs.Directx;
+            storageMin_TextBlock.Text = GameMinSysReqs.Storage;
+
+            osMax_TextBlock.Text = GameMaxSysReqs.Os;
+            cpuMax_TextBlock.Text = GameMaxSysReqs.Cpu;
+            memMax_TextBlock.Text = GameMaxSysReqs.Memory;
+            gpuMax_TextBlock.Text = GameMaxSysReqs.Gpu;
+            directxMax_TextBlock.Text = GameMaxSysReqs.Directx;
+            storageMax_TextBlock.Text = GameMaxSysReqs.Storage;
+
+            buyGame_Label.Content = $"Купить {Game.Name}";
+            buyGame_Button.Content = $"{Game.UsdPrice} руб.";
+
+            string videoFilePath = SaveVideoToFile(GameVideos.First(), "video.mp4");
             PlayVideo(videoFilePath);
+            video_MediaElement.Play();
+            Thread.Sleep(2);
+            video_MediaElement.Pause();
+        }
+
+        public void RefreshMedia()
+        {
+            foreach (var video in GameVideos)
+            {
+                var theGrid = new Grid();
+                var theVideo = new MediaElement();
+                var playImage = new Image();
+                playImage.Source = new BitmapImage(new Uri("pack://application:,,,/Assets/preview-play.png"));
+                playImage.Height = 30;
+                playImage.Width = 30;
+                playImage.VerticalAlignment = VerticalAlignment.Center;
+                playImage.HorizontalAlignment = HorizontalAlignment.Center;
+                theGrid.Children.Add(theVideo);
+                theGrid.Children.Add(playImage);
+                theGrid.MouseEnter += Screenshot_MouseEnter;
+                theGrid.MouseLeave += Screenshot_MouseLeave;
+                theVideo.PreviewMouseLeftButtonDown += VideoSmall_PreviewMouseLeftButtonDown;
+                playImage.PreviewMouseLeftButtonDown += (sender, e) => PlayImage_PreviewMouseLeftButtonDown(sender,e, theVideo);
+
+                media_StackPanel.Children.Add(theGrid);
+                
+                videosPathMini.Add(SaveVideoToFile(video, $"{Guid.NewGuid()}.mp4"));
+                theVideo.Source = new Uri(videosPathMini.Last());
+                theVideo.LoadedBehavior = MediaState.Manual;
+                theVideo.UnloadedBehavior = MediaState.Manual;
+                theVideo.Play();
+                Thread.Sleep(2);
+                theVideo.Pause();
+                theVideo.Margin = new Thickness(0);
+
+                if (video == GameVideos.First())
+                    theGrid.Margin = new Thickness(0,10,0,10);
+
+            }
+            foreach (var image in GameImages)
+            {
+                var theImage = new Image();
+                theImage.Source = ImageHelper.GetImage(image);
+                RenderOptions.SetBitmapScalingMode(theImage, BitmapScalingMode.HighQuality);
+
+                theImage.PreviewMouseLeftButtonDown += Screenshot_PreviewMouseLeftButtonDown;
+                theImage.MouseEnter += Screenshot_MouseEnter;
+                theImage.MouseLeave += Screenshot_MouseLeave;
+
+                media_StackPanel.Children.Add(theImage);
+            }
+        }
+
+        private void TheGrid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlayImage_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e, MediaElement me)
+        {
+            VideoSmall_PreviewMouseLeftButtonDown(me, e);
+        }
+
+        private void VideoSmall_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            video_MediaElement.Source = ((MediaElement)sender).Source;
+            screenshot_Image.Visibility = Visibility.Collapsed;
+            video_MediaElement.Visibility = Visibility.Visible;
+            video_Slider.Visibility = Visibility.Visible;
+            videoControls_Grid.Visibility = Visibility.Visible;
+            play_Button_Click(null, null);
+        }
+
+        public string SaveVideoToFile(byte[] videoData, string fileName)
+        {
+            string filePath = Path.Combine(Path.GetTempPath(), fileName);
+            File.WriteAllBytes(filePath, videoData);
+            return filePath;
+        }
+
+        private void PlayVideo(string videoFilePath)
+        {
+            video_MediaElement.Source = new Uri(videoFilePath);
+            video_MediaElement.LoadedBehavior = MediaState.Manual;
+            video_MediaElement.UnloadedBehavior = MediaState.Manual;
+            video_MediaElement.MediaEnded += (sender, e) => video_MediaElement.Stop();
+            video_MediaElement.Volume = 1;
+        }
+
+        private void pause_Button_Click(object sender, RoutedEventArgs e)
+        {
+            video_MediaElement.Pause();
+            timer.Stop();
+            play_Button.Visibility = Visibility.Visible;
+            pause_Button.Visibility = Visibility.Collapsed;
+        }
+
+        private void play_Button_Click(object sender, RoutedEventArgs e)
+        {
+            video_MediaElement.Play();
+            timer.Start();
+            pause_Button.Visibility = Visibility.Visible;
+            play_Button.Visibility = Visibility.Collapsed;
+        }
+
+        private void video_Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (isDragging)
+            {
+                video_MediaElement.Position = TimeSpan.FromSeconds(video_Slider.Value);
+            }
+        }
+
+        private void video_Slider_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = true;
+            timer.Stop();
+            video_MediaElement.Pause();
+        }
+
+        private void video_Slider_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+            video_MediaElement.Position = TimeSpan.FromSeconds(video_Slider.Value);
+            video_MediaElement.Play();
+            timer.Start();
+        }
+
+        private void video_MediaElement_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            video_Slider.Maximum = video_MediaElement.NaturalDuration.TimeSpan.TotalSeconds;
+        }
+
+        private void video_MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            video_MediaElement.Stop();
+            timer.Stop();
+            video_Slider.Value = 0;
+            play_Button.Visibility = Visibility.Visible;
+            pause_Button.Visibility = Visibility.Collapsed;
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            video_MediaElement.Stop();
+            video_MediaElement.Close();
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        private void volume_Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            volume_Slider.Visibility = Visibility.Visible;
+        }
+
+        private void volume_Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            video_MediaElement.Volume = volume_Slider.Value / 100;
+        }
+
+        private void volume_Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (!volume_Slider.IsMouseOver)
+                volume_Slider.Visibility = Visibility.Collapsed;
+        }
+
+        private void volume_Slider_MouseLeave(object sender, MouseEventArgs e)
+        {
+            volume_Slider.Visibility = Visibility.Collapsed;
+        }
+        private void Screenshot_MouseLeave(object sender, MouseEventArgs e)
+        {
+            this.Cursor = null;
+        }
+
+        private void Screenshot_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+        }
+        private void Screenshot_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            video_MediaElement.Visibility = Visibility.Collapsed;
+            video_MediaElement.Close();
+            videoControls_Grid.Visibility = Visibility.Collapsed;
+            video_Slider.Visibility = Visibility.Collapsed;
+            screenshot_Image.Visibility = Visibility.Visible;
+            screenshot_Image.Source = ((Image)sender).Source;
         }
 
         private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -52,31 +282,26 @@ namespace Mist.Pages.MainWindowPages
             var margin = new Thickness(m, 15, m, 0);
             main_Grid.Margin = margin;
         }
-        public string SaveVideoToFile(byte[] videoData, string fileName)
-        {
-            string filePath = Path.Combine(Path.GetTempPath(), fileName);
-            File.WriteAllBytes(filePath, videoData);
 
-            return filePath;
-        }
-        private void PlayVideo(string videoFilePath)
+        private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            video_MediaElement.Source = new Uri(videoFilePath);
-            video_MediaElement.LoadedBehavior = MediaState.Manual;
-            video_MediaElement.UnloadedBehavior = MediaState.Manual;
-            video_MediaElement.MediaEnded += (sender, e) => video_MediaElement.Stop();
-            video_MediaElement.Volume = 1;
-            video_MediaElement.Play();
+            
         }
 
-        private void pause_Button_Click(object sender, RoutedEventArgs e)
+        private void media_ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
         {
-            video_MediaElement.Pause();
-        }
-
-        private void play_Button_Click(object sender, RoutedEventArgs e)
-        {
-            video_MediaElement.Play();
+            if (e.Delta > 0)
+            {
+                media_ScrollViewer.LineLeft();
+                media_ScrollViewer.LineLeft();
+            }
+            else
+            {
+                media_ScrollViewer.LineRight();
+                media_ScrollViewer.LineRight();
+            }
+                
+            e.Handled = true;
         }
     }
 }
